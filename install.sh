55b3979b32097459a895a5aedd32da5be2841e2c
tyf1996 <tyf.liking@gmail.com>
2020-01-26 10:18:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 apt-get update
 wget https://install.direct/go.sh && bash go.sh
 mv /etc/v2ray/config.json ./config.json.bak
-mv ./config.json /etc/v2ray/config.json
+cp ./config.json /etc/v2ray/config.json
 systemctl stop v2ray
 systemctl start v2ray
 bash ./tcp.sh
